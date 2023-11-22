99b2dde991846726d6f214a25a2cf120f95dcd7f
Baker Alazzawi <Baker.alazzawi0@gmail.com>
2023-11-22 11:13:02 +0300

    Update Tbl.jsx

--- admin/src/Components/Container/Table/Tbl.jsx
+++ admin/src/Components/Container/Table/Tbl.jsx
@@ -5,30 +5,33 @@ const { Column } = Table;
 import Edit from "../Modal/Edit";
 import Delete from "../Modal/Delete";
 const Table1 = ({ products }) => (
-  <Table1 dataSource={products}>
-    <Column title="ID" dataIndex="id" key="id" />
-    <Column title="Title" dataIndex="title" key="title" />
-    <Column title="Description" dataIndex="description" key="description" />
-    <Column title="Price" dataIndex="price" key="price" />
-    <Column
-      title="Action"
-      key="action"
-      render={(_, record) => (
-        <Space size="middle">
-          <a>
-            <Edit
-              id={record.id}
-              onEdit={handleEditSuccess}
-              products={products}
-            />
-          </a>
-          <a>
-            <Delete id={record.id} onDelete={handleDeleteSuccess} />
-          </a>
-        </Space>
-      )}
-    />
-  </Table1>
+  <div className="table-1">
+    <Table dataSource={products}>
+      <Column title="ID" dataIndex="id" key="id" />
+      <Column title="Title" dataIndex="title" key="title" />
+      <Column title="Description" dataIndex="description" key="description" />
+      <Column title="Price" dataIndex="price" key="price" />
+      <Column
+        title="Action"
+        key="action"
+        render={(_, record) => (
+          <Space size="middle">
+            <a>
+              <Edit
+                id={record.id}
+                onEdit={handleEditSuccess}
+                products={products}
+              />
+            </a>
+            <a>
+              <Delete id={record.id} onDelete={handleDeleteSuccess} />
+            </a>
+          </Space>
+        )}
+      />
+    </Table>
+  </div>
+  
 );
   const handleEditSuccess = () => {
     alert("Product edited successfully");
